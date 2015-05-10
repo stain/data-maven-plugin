0c2d79803968ba608fd7ab0a2c1b089bbded125b
Stian Soiland-Reyes <soiland-reyes@cs.manchester.ac.uk>
2015-05-10 22:36:12 +0100

    data-maven-plugin

---
+++ src/main/java/org/openphacts/data_maven_plugin/AddDataMojo.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2015 University of Manchester
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openphacts.data_maven_plugin;
+
+import java.io.File;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+/**
+ * Mojo for adding data/ resources
+ *
+ */
+@Mojo(name = "add", defaultPhase = LifecyclePhase.GENERATE_RESOURCES)
+public class AddDataMojo extends AbstractMojo {
+	@Parameter(defaultValue = "${project.build.directory}", property = "outputDir", required = true)
+	private File outputDirectory;
+
+	public void execute() throws MojoExecutionException {
+	}
+}
