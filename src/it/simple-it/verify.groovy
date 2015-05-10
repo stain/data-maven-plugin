8baf8126c793a2fb863d2862dc8649cdf4ba61f3
Stian Soiland-Reyes <soiland-reyes@cs.manchester.ac.uk>
2015-05-10 23:31:36 +0100

    First draft of data-maven-plugin
    This simply adds the data/ folder as
    data/ on the classpath (e.g. to JAR)

--- src/it/simple-it/verify.groovy
+++ src/it/simple-it/verify.groovy
@@ -1,3 +1,5 @@
-File touchFile = new File( basedir, "target/touch.txt" );
+File file1 = new File( basedir, "target/classes/data/file1.txt" );
+File file2 = new File( basedir, "target/classes/data/file2.txt" );
 
-assert touchFile.isFile()
+assert file1.isFile()
+assert file2.isFile()
--- src/main/java/org/openphacts/data_maven_plugin/AddDataMojo.java
+++ src/main/java/org/openphacts/data_maven_plugin/AddDataMojo.java
@@ -17,11 +17,14 @@ package org.openphacts.data_maven_plugin;
 
 import java.io.File;
 
+import org.apache.maven.execution.MavenSession;
+import org.apache.maven.model.Resource;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
 
 /**
  * Mojo for adding data/ resources
@@ -32,6 +35,28 @@ public class AddDataMojo extends AbstractMojo {
 	@Parameter(defaultValue = "${project.build.directory}", property = "outputDir", required = true)
 	private File outputDirectory;
 
+	@Parameter(defaultValue = "data", property="dataDir", required=true)
+	private File dataDirectory;
+
+	@Parameter(defaultValue="data", property="targetPath", required=true)
+	private String targetPath;
+
+    @Parameter( defaultValue = "${session}", readonly = true )
+    private MavenSession session;
+
+    @Parameter( defaultValue = "${project}", readonly = true )
+    private MavenProject project;
+
+
 	public void execute() throws MojoExecutionException {
+		if (! dataDirectory.isDirectory()) {
+			getLog().debug("Not a directory: " + dataDirectory);
+			return;
+		}
+		getLog().info("Adding data from: " + dataDirectory);
+		Resource resource = new Resource();
+		resource.setDirectory(dataDirectory.getPath());
+		resource.setTargetPath(targetPath);
+		project.addResource(resource);
 	}
 }
