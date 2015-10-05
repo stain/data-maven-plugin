14f10fe21255672a431e4aa88875bf5ce088de40
Stian Soiland-Reyes <soiland-reyes@cs.manchester.ac.uk>
2015-10-05 15:18:55 +0100

    test both data/ and src/main/resources

--- src/it/data-packaging/verify.groovy
+++ src/it/data-packaging/verify.groovy
@@ -1,2 +1,14 @@
-File file1 = new File( basedir, "target/classes/data/dataplugin-example/test.txt" );
-assert file1.isFile()
+import org.apache.taverna.robundle.Bundle;
+import org.apache.taverna.robundle.Bundles;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+
+File zip = new File( basedir, "target/dataplugin-example-0.0.1-SNAPSHOT.bundle.zip" );
+assert zip.isFile();
+
+
+Bundle bundle = Bundles.openBundleReadOnly(zip.toPath());
+assert Files.isRegularFile(bundle.getPath("data/dataplugin-example/test.txt"));
+assert Files.isRegularFile(bundle.getPath("data/dataplugin-example/other.txt"));
+bundle.close();
