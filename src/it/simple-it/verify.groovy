3236b1718137af442bba291503b98b4dd701cd14
Stian Soiland-Reyes <soiland-reyes@cs.manchester.ac.uk>
2015-10-05 02:50:07 +0100

    no need to test file2.txt

--- src/it/simple-it/verify.groovy
+++ src/it/simple-it/verify.groovy
@@ -1,5 +1,3 @@
 File file1 = new File( basedir, "target/classes/data/file1.txt" );
-File file2 = new File( basedir, "target/classes/data/file2.txt" );
-
 assert file1.isFile()
-assert file2.isFile()
+
